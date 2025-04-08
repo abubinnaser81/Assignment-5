377525b68b6455f24602989b917fb5805e7ab9cf
abubinnaser81 <abunaser50032@gmail.com>
2025-04-08 21:15:44 +0600

    lastt

--- script/toggle.js
+++ script/toggle.js
@@ -7,15 +7,3 @@ function inputValue(id){
 function setInnerText(id,value){
     document.getElementById(id).innerText = value ; 
 }
-
-// update time
-// function updateTime() {
-//     const date = new Date();
-//     return date.toLocaleTimeString('en-US', { 
-//         hour: '2-digit', 
-//         minute: '2-digit', 
-//         second: '2-digit', 
-//         hour12: true 
-//     });
-
-// }
